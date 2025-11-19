ba96d68913e6920c309486b5315cdd474fd8a2f6
meaninglesshexcode <somebodyelse2009@gmail.com>
2025-11-19 00:23:49 -0800

    Update cornol_entry.cbl
    Yippee!

--- kernel/cornol_entry.cbl
+++ kernel/cornol_entry.cbl
@@ -1,7 +1,16 @@
->This is a TEST! This is NOT a kernel!!!
+>This is a TEST! This is NOT an OS!!!
 IDENTIFICATION DIVISION.
 PROGRAM-ID. cornol_entry.
 
+ENVIRONMENT DIVISION.
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    01 keyboard-buffer PIC X(1).  *> Will hold one byte
+
 PROCEDURE DIVISION.
-DISPLAY 'Hello, World!'.
-STOP RUN.
+MAIN-LOOP.
+    PERFORM UNTIL 0 = 1
+        CALL 'check_keyboard' USING keyboard-buffer
+        DISPLAY "Key read: " keyboard-buffer
+    END-PERFORM
+    STOP RUN.
