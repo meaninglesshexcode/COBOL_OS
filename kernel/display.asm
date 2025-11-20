ca0a9af5be16bd0530bcd879d08c79785f57a8a0
meaninglesshexcode <somebodyelse2009@gmail.com>
2025-11-20 11:40:25 -0800

    Update display.asm
    Fixed to run with the new cornol_entry.cbl

--- kernel/display.asm
+++ kernel/display.asm
@@ -1,5 +1,4 @@
 format ELF64
-
 public display_text
 
 section '.text' executable
@@ -7,20 +6,26 @@ display_text:
     push rbx
     push rsi
     push rdi
+    
+    ; Clear screen
     mov rdi, 0xB8000
     mov rcx, 80 * 25
     mov ax, 0x0720
     rep stosw
-    mov rsi, [rsp + 24]
+    
+    ; RDI contains pointer to string (passed by COBOL)
+    mov rsi, rdi        ; Use the passed pointer, not stack
     mov rdi, 0xB8000
     mov ah, 0x07
-print_loop:
+    
+.print_loop:
     lodsb
     test al, al
-    jz done
+    jz .done
     stosw
-    jmp print_loop
-done:
+    jmp .print_loop
+    
+.done:
     pop rdi
     pop rsi
     pop rbx
