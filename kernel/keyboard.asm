997e67100eb8de11bedd209b945e7106b158ba89
meaninglesshexcode <somebodyelse2009@gmail.com>
2025-11-20 11:31:44 -0800

    Update keyboard.asm
    Made a HORRENDOUS mistake and accidentally declared two keyboard_buffer objects in both languages.

--- kernel/keyboard.asm
+++ kernel/keyboard.asm
@@ -1,18 +1,18 @@
 format ELF64
-
 public check_keyboard
-public keyboard_buffer
-
-section '.data' writeable
-keyboard_buffer: times 256 db 0
 
 section '.text' executable
 check_keyboard:
-    mov al, 0
+    ; RDI contains pointer to COBOL's buffer (System V calling convention)
+    push rdi           ; save the pointer
+    
 .wait_input:
     in al, 0x64        ; PS/2 status port
     test al, 1         ; check if output buffer full
     jz .wait_input
-    in al, 0x60        ; read key
-    mov [keyboard_buffer], al
+    
+    in al, 0x60        ; read the key
+    
+    pop rdi            ; restore pointer
+    mov [rdi], al      ; write key to COBOL's buffer
     ret
