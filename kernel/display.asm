03489258f43f17226db8159b5a17cb6bd2b6ce1d
meaninglesshexcode <somebodyelse2009@gmail.com>
2026-01-18 20:54:29 -0800

    Refactor display_text in display.asm
    Refactor display_text function to make it actually work

--- kernel/display.asm
+++ kernel/display.asm
@@ -2,29 +2,34 @@ format ELF64
 public display_text
 
 section '.text' executable
+
 display_text:
     push rbx
     push rsi
     push rdi
-    
+
+    cld                     ; ALWAYS do this before lods/stos
+
+    ; Save string pointer before clobbering RDI
+    mov rsi, rdi            ; RSI = string pointer
+
     ; Clear screen
     mov rdi, 0xB8000
     mov rcx, 80 * 25
     mov ax, 0x0720
     rep stosw
-    
-    ; RDI contains pointer to string (passed by COBOL)
-    mov rsi, rdi        ; Use the passed pointer, not stack
+
+    ; Print string
     mov rdi, 0xB8000
     mov ah, 0x07
-    
+
 .print_loop:
-    lodsb
+    lodsb                   ; AL = *RSI++
     test al, al
     jz .done
-    stosw
+    stosw                   ; write char+attr
     jmp .print_loop
-    
+
 .done:
     pop rdi
     pop rsi
