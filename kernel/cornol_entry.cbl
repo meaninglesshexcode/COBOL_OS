78c4c19e9eb62e8470bd62915a17b8f2d2014abe
meaninglesshexcode <somebodyelse2009@gmail.com>
2026-01-18 21:06:00 -0800

    Remade all of cornol
    Pain and suffering

--- kernel/cornol_entry.cbl
+++ kernel/cornol_entry.cbl
@@ -1,19 +1,23 @@
->This is a TEST! This is NOT an OS!!!
 IDENTIFICATION DIVISION.
 PROGRAM-ID. cornol_entry.
-ENVIRONMENT DIVISION.
+
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-    01 keyboard-buffer PIC X(1).
-    01 display-message PIC X(20) VALUE "Key read: ".
-    01 full-message PIC X(21).
+    01 keyboard-buffer PIC X.
+    01 full-message.
+        05 msg PIC X(21).
+        05 nul PIC X VALUE X"00".
+
 PROCEDURE DIVISION.
 MAIN-LOOP.
-    PERFORM UNTIL 0 = 1
-        CALL 'check_keyboard' USING keyboard-buffer
-        STRING display-message DELIMITED BY SIZE
-               keyboard-buffer DELIMITED BY SIZE
-               INTO full-message
-        CALL 'display_text' USING full-message
+    PERFORM UNTIL 1 = 0
+        CALL "check_keyboard" USING keyboard-buffer
+
+        MOVE SPACES TO msg
+        MOVE "Key read: " TO msg(1:10)
+        MOVE keyboard-buffer TO msg(11:1)
+
+        CALL "display_text" USING full-message
     END-PERFORM
-    STOP RUN.
+
+    GOBACK.
