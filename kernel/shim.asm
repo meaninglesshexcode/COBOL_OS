8a44c26e97eb7a9b78835388e95c9e5bc541c754
meaninglesshexcode <somebodyelse2009@gmail.com>
2026-01-18 20:59:09 -0800

    Make shim.asm work
    Adjust stack pointer and align for SysV ABI before calling entry function, fixes fatal errors

--- kernel/shim.asm
+++ kernel/shim.asm
@@ -3,8 +3,16 @@ entry start
 
 segment readable executable
 start:
-    mov rsp, stack_space
-    jmp cornol_entry
+    mov rsp, stack_space + 4096
+    and rsp, -16
+    sub rsp, 8            ; SysV ABI alignment
+
+    call cornol_entry
+
+.hang:
+    hlt
+    jmp .hang
 
 segment readable writeable
+align 16
 stack_space: times 4096 db 0
